df7c3daac992aa3dd1e52757955b02fb070bba04
Omkar Bhagat <omkarbhagat93+github@gmail.com>
2023-09-27 22:14:19 +0100

    lc 21

---
+++ 0021_mergetwosortedlists/0021_mergetwosortedlists.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+
+	return dummy.Next
+}
+
+func printList(head *ListNode) {
+	current := head
+	for current != nil {
+		fmt.Printf("%d -> ", current.Val)
+		current = current.Next
+	}
+	fmt.Println("nil")
+}
+
+func main() {
+	// Create two sorted linked lists
+	l1 := &ListNode{1, &ListNode{2, &ListNode{4, nil}}}
+	l2 := &ListNode{1, &ListNode{3, &ListNode{4, &ListNode{5, &ListNode{6, nil}}}}}
+
+	fmt.Println("List 1:")
+	printList(l1)
+
+	fmt.Println("List 2:")
+	printList(l2)
+
+	merged := mergeTwoLists(l1, l2)
+
+	fmt.Println("Merged List:")
+	printList(merged)
+}
