28a0b4a845545daf8cc834df705f6ce7e87564f0
Omkar Bhagat <omkarbhagat93+github@gmail.com>
2023-10-02 22:30:42 +0100

    lc 67

---
+++ 0067_addbinary/0067_addbinary.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func addBinary(a string, b string) string {
+	result := ""
+	carry := 0
+	i, j := len(a)-1, len(b)-1
+
+	for i >= 0 || j >= 0 || carry > 0 {
+		sum := carry
+		if i >= 0 {
+			sum += int(a[i] - '0')
+			i--
+		}
+		if j >= 0 {
+			sum += int(b[j] - '0')
+			j--
+		}
+
+		result = fmt.Sprintf("%d%s", sum%2, result)
+		carry = sum / 2
+	}
+
+	return result
+}
+
+func main() {
+	a := "1010"
+	b := "1011"
+	sum := addBinary(a, b)
+	fmt.Printf("Binary Sum: %s\n", sum)
+}
